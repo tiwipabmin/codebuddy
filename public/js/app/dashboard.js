7b850f3ade6157339ddb9790429f8750759953b2
tharamalai <57070054@kmitl.ac.th>
2017-12-07 13:11:38 +0700

    fix item layout

--- public/js/app/dashboard.js
+++ public/js/app/dashboard.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
             $(".user-list").empty();
             if (data.length > 0) {
                 data.forEach(function(user) {
-                    $(".user-list").append("<div class='item'><div class='right floated content'><div class='ui button add-user-button' onclick='onClickAddUserButton(\"" +user.username+"\", \"" +user.username+"\", \"" +user.username+"\")'>Add</div></div><img class='ui avatar image' src='/images/christian.jpg'><div class='content'><div class='header'>"+user.username+"</div></div></div>");
+                    $(".user-list").append("<div class='item'><div class='right floated content'><div class='ui button add-user-button' onclick='onClickAddUserButton(\"" +user.username+"\", \"" +user.username+"\", \"" +user.username+"\")'>Add</div></div><img class='ui avatar image' src='/images/christian.jpg'><div class='content'><div class='header'>"+user.username+"</div><div class='description'><div class='ui circular labels'><a class='ui teal label'>score "+parseFloat(user.avgScore).toFixed(2)+"</a></div></div></div></div>");
                 }, this);  
             } else {
                 $(".user-list").append("<li class='ui item'>No results</li>")
