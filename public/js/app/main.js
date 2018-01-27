74566d09f7fb1a3b4dfab45f27a734efbb0f7f1f
Unknown <s7070068@kmitl.ac.th>
2018-01-27 16:15:25 +0700

    Count when detect

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -1,5 +1,6 @@
 // timer
-
+var sec=Date.now()
+var counts=0;
 const startTimer = function (duration, display, changeRoleBtn) {
   const start = Date.now()
   let { diff, minutes, seconds } = 0
@@ -40,36 +41,36 @@ window.onload = function() {
   var context = canvas.getContext('2d');
 
   var tracker = new tracking.ObjectTracker('face');
-  tracker.setInitialScale(4);
-  tracker.setStepSize(2);
+  tracker.setInitialScale(1);
+  tracker.setStepSize(1);
   tracker.setEdgesDensity(0.1);
-
   tracking.track('#video', tracker, { camera: true });
 
-  var sec = 0;
-  tracker.on('track', function(event) {
-    
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    event.data.forEach(function(rect) {
-      context.strokeStyle = '#a64ceb';
-      context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-      context.font = '11px Helvetica';
-      context.fillStyle = "#fff";
-      context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-      context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-
-      
+  setInterval( function(){
+    var flag = 0;
+    tracker.on('track', function(event) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      event.data.forEach(function(rect) {
         
-      function pad ( val ) { return val > 9 ? val : "0" + val; }
-      setInterval( function(){
-          document.getElementById("secs").innerHTML=pad(++sec%60);
-          document.getElementById("mins").innerHTML=pad(parseInt(sec/60,10));
-      }, 1000);
-
-      // document.getElementById('my_timer').innerHTML = count
-      
-      
-    });
+        context.strokeStyle = '#a64ceb';
+        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+        context.font = '11px Helvetica';
+        context.fillStyle = "#fff";
+        context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+        context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+        
+        if(flag == 0){
+          flag = 1;
+          counts++;
+          document.getElementById("secs").innerHTML=pad(((Date.now()-sec)/1000)%60);
+          document.getElementById("mins").innerHTML=pad(parseInt(((Date.now()-sec)/1000)/60,10));
+          document.getElementById("counts").innerHTML=counts;
+        }
+  
+        // document.getElementById('my_timer').innerHTML = count
+      });
+    }, 1000);
+ 
   });
 
   var gui = new dat.GUI();
@@ -77,6 +78,28 @@ window.onload = function() {
   gui.add(tracker, 'initialScale', 1.0, 10.0).step(0.1);
   gui.add(tracker, 'stepSize', 1, 5).step(0.1);
 };
+var time;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+function counter(){
+      time = setInterval( function(){
+      document.getElementById("secs").innerHTML=pad(++sec%60);
+      document.getElementById("mins").innerHTML=pad(parseInt(sec/60,10));
+  }, 1000); 
+  console.log("hi")
+  timeout()
+}
 
+function timeout(){ 
+  var timeout = setInterval( function(){
+      time = setInterval( function(){
+        counts
+        document.getElementById("secs").innerHTML=pad(++sec%60);
+        document.getElementById("mins").innerHTML=pad(parseInt(sec/60,10));
+        console.log("hi")
+    }, 1000);
+    clearInterval(time)
+    console.log("stop")
+  }, 300000);
+}
 
 
