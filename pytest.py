da52efb015c1efb88f7c9fd6ef01e4836e37442b
tharamalai <57070054@kmitl.ac.th>
2018-02-20 15:10:35 +0700

    can count

--- pytest.py
+++ pytest.py
@@ -1,29 +1,5 @@
-'''aaaa'''
-#!/usr/bin/python
-#
-# Python program to demonstrate for loops.
-#
-
-# The for loop goes through a list, like foreach in
-# some other languages.  A useful construct.
-for x in ['Bill', 'Alice', 'Joe', 'Sue']:
-    print x, 'likes jelly beans.'
-
-# The range operator simply creates a list of numbers
-# in the indicated range.  Note that the range ends
-# before the second argument.
-print range(5, 10)
-
-# Range works with for to create the traditional for loop.
-for y in range(2, 10):
-    print y,
-print
-
-for y in range(2, 10, 2):
-    print y,
-print
-
-for y in range(20, 10, -1):
-    print y,
-print
-    
+aaa=0
+while(1):
+    print "hello"
+    aaa += 1
+    print aaa
