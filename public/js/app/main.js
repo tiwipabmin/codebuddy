1f6a98360f5e65e362aab202bcdd336f8f58d93f
Unknown <s7070068@kmitl.ac.th>
2018-03-10 23:56:00 +0700

    Chat toggle up down

--- public/js/app/main.js
+++ public/js/app/main.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 	$('#live-chat header').on('click', function() {
 		$('.chat').slideToggle(300, 'swing');
 		$(".chat-history").animate({ scrollTop: $('.message-list').height() }, "fast");
+		$("#angle-up").toggleClass("rotate");
 	});
 
 });
