d5b46f71b546875e40fed69609f71ba5f47ac396
tiwipabmin <tiwipabmin@gmail.com>
2019-07-26 18:21:53 +0700

    Refactoring code in playgroundRemote.js

--- public/js/app/playgroundRemote.js
+++ public/js/app/playgroundRemote.js
@@ -636,10 +636,6 @@ socket.on('update message', (payload) => {
   if (payload.user._id === uid) {
     $(".message-list").append("<li class='ui item'><a class='ui avatar image'></a><div class='content'></div><div class='description curve-box-user'><p>"+ payload.message.message +"</p></div></li>");
     $("#inputMessage").val("")
-    // socket.emit('is typing', {
-    //   uid: uid,
-    //   text: ''
-    // })
   } else {
     $(".message-list").append("<li class='ui item'><a class='ui avatar image'><img src='"+ payload.user.img +"'></a><div class='description curve-box'><p>"+ payload.message.message +"</p></div></li>");
   }
@@ -675,6 +671,7 @@ function videoEvent(b) {
     webrtc.resumeVideo();
   }
 }
+
 // attach ready event -- Video Toggle
 $(document)
   .ready(function () {
@@ -692,71 +689,31 @@ $(document)
           active: '<i class="unmute icon"/>'
         }
       });
-    // $('#inputMessage').keydown(function() {
-    //   socket.emit('is typing', {
-    //     uid: uid,
-    //     text: `${user} is typing...`
-    //   })
-    // });
-    // $('#inputMessage').keyup(function() {
-    //   socket.emit('is typing', {
-    //     uid: uid,
-    //     text: ''
-    //   })
-    // });
-    $('#inputMessage').change(function() {
-      if($('#inputMessage').val() != "") {
-        console.log("is typing")
+    $('#inputMessage').keydown(function() {
+      socket.emit('is typing', {
+        uid: uid,
+        text: `${user} is typing...`
+      })
+      clearTimeout(parseInt($('#inputMessage').attr('data-timeId')))
+    });
+    $('#inputMessage').keyup(function() {
+      let timeId = setTimeout(function(){
         socket.emit('is typing', {
-              uid: uid,
-              text: `${user} is typing...`
-            })
-      } else {
-        socket.emit('is typing', {
-              uid: uid,
-              text: ''
-            })
-      }
+            uid: uid,
+            text: ''
+        })
+      }, 5000)
+      $('#inputMessage').attr('data-timeId', timeId)
     });
     $('#inputMessage').keydown(function(e) {
       if (e.keyCode == 13) {
         sendMessage()
       }
     });
-    console.log("is typing : " + $('#inputMessage').val())
     updateScroll();
   });
-$(document)
-.ready(function () {
-  if($('#inputMessage').val() != "") {
-    console.log("is typing")
-    socket.emit('is typing', {
-          uid: uid,
-          text: `${user} is typing...`
-        })
-  } else {
-    socket.emit('is typing', {
-          uid: uid,
-          text: ''
-        })
-  }
-});
 
 $(function(){
-
-  console.log("is typing : " + $('#inputMessage').val())
-  if($('#inputMessage').val() != "") {
-    console.log("is typing")
-    socket.emit('is typing', {
-          uid: uid,
-          text: `${user} is typing...`
-        })
-  } else {
-    socket.emit('is typing', {
-          uid: uid,
-          text: ''
-        })
-  }
 
   var acc = 0;
   var session_flag = 0;
@@ -796,38 +753,10 @@ $(function(){
   }, 10000)
 });
 
-console.log("is typing : " + $('#inputMessage').val())
-if($('#inputMessage').val() != "") {
-  console.log("is typing")
-  socket.emit('is typing', {
-        uid: uid,
-        text: `${user} is typing...`
-      })
-} else {
-  socket.emit('is typing', {
-        uid: uid,
-        text: ''
-      })
-}
-
-$('.ui.video.toggle.button')
-  .on('click', handler.activate);
-$('.ui.video.toggle.button')
-  .state({
-    text: {
-      inactive: '<i class="pause circle icon"/>',
-      active: '<i class="play video icon"/>'
-    }
-  });
-$('.ui.mute.toggle.button')
-  .on('click', handler.activate);
-$('.ui.mute.toggle.button')
-  .state({
-    text: {
-      inactive: '<i class="mute icon"/>',
-      active: '<i class="unmute icon"/>'
-    }
-  });
+// $('.ui.video.toggle.button')
+//   .on('click', handler.activate);
+// $('.ui.mute.toggle.button')
+//   .on('click', handler.activate);
 
 function switchRole() {
   socket.emit('switch role', {
